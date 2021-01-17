97f454fcc26deb1101b54c4ec214f429ab6813d1
codolog <77565375+codolog@users.noreply.github.com>
2021-01-17 12:22:17 +0400

    Add files via upload

---
+++ main57.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,k=0;
+    double s=0;
+    cin >>a;
+    while (a!=0){
+            k++;
+            s=s+a
+            cin>>a;
+    }
+    cout << s/k << endl;
+    return 0;
+}
